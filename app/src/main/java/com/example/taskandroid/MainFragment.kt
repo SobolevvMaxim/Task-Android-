66c28e61fcd1820cd2c28697c17552987f5d3259
MainComp <sobolevvw@gmail.com>
2022-01-28 02:31:20 +0300

    multiply select todo

--- app/src/main/java/com/example/taskandroid/MainFragment.kt
+++ app/src/main/java/com/example/taskandroid/MainFragment.kt
@@ -1,20 +1,20 @@
 package com.example.taskandroid
 
+import android.R.attr.data
+import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.main_fragment.*
 import java.util.*
+
 
 class MainFragment : Fragment(R.layout.main_fragment) {
     companion object {
@@ -34,6 +34,24 @@ class MainFragment : Fragment(R.layout.main_fragment) {
             )
         }
 
+//    private val imageLauncher =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//            if (result.resultCode == Activity.RESULT_OK) {
+//                val data: Intent? = result.data
+//                val flags = data?.flags?.and(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//                val resolver = activity?.contentResolver
+//
+//            }
+//        }
+
+//    private val getPhotos =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//            if (result.resultCode == Activity.RESULT_OK) {
+//                val data = result.data
+//                // TODO: maybe select multiple images from gallery
+//            }
+//        }
+
     private val takeFrags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION
 
     private val viewModel = viewModels<MainViewModel>()
@@ -43,7 +61,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         setRecyclerView()
 
         if (savedInstanceState == null) {
-            viewModel.value.loadImagesFromBase()
+//            viewModel.value.loadImagesFromBase()
         }
 
         viewModel.value.photosLiveData.observe(viewLifecycleOwner) { images ->
@@ -57,13 +75,13 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
         add_button.setOnClickListener {
             getPhoto.launch("image/*")
+//            getMultiplyImages()
         }
     }
 
     private fun updateRecyclerView(images: List<Image>) {
+        if (images.isNullOrEmpty()) return
         context?.contentResolver?.apply {
-            takePersistableUriPermission(images[0].uri, takeFrags)
-
             photosAdapter.submitList(images.map { it.toImageItem(this) })
         }
     }
@@ -78,4 +96,22 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
         userRecycle.adapter = photosAdapter
     }
+
+//    fun getImages(images: List<Image>) {
+//        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+//            putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+//            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//            type = "image/*"
+//        }
+//        imageLauncher.launch(intent)
+//    }
+//
+//    fun getMultiplyImages() {
+//        Intent().apply {
+//            type = "image/*"
+//            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+//            action = Intent.ACTION_GET_CONTENT
+//            getPhotos.launch(this)
+//        }
+//    }
 }
