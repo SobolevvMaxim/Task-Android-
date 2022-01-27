6f4b4ff5572757726328a3c57580bd1fdb01aed9
MainComp <sobolevvw@gmail.com>
2022-01-27 15:51:56 +0300

    get image from gallery done

---
+++ app/src/main/java/com/example/taskandroid/ImageItem.kt
@@ -0,0 +1,5 @@
+package com.example.taskandroid
+
+import android.net.Uri
+
+data class ImageItem(val uri: Uri)
--- app/src/main/java/com/example/taskandroid/MainFragment.kt
+++ app/src/main/java/com/example/taskandroid/MainFragment.kt
@@ -2,18 +2,28 @@ package com.example.taskandroid
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment(R.layout.main_fragment) {
     companion object {
         fun newInstance() = MainFragment()
     }
 
+    private val getPhoto =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            ImageItem(uri)
+        }
+
     private val viewModel = viewModels<MainViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        add_button.setOnClickListener {
+            getPhoto.launch("image/*")
+        }
     }
 }
