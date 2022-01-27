8778dfb48ec4c6da4f3349ccb71f26877f24bc81
MainComp <sobolevvw@gmail.com>
2022-01-27 13:18:42 +0300

    Main Fragment and Activity setup

--- app/src/main/java/com/example/taskandroid/MainActivity.kt
+++ app/src/main/java/com/example/taskandroid/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.taskandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            navigateTo(MainFragment.newInstance(), addToBackStack = false)
+        }
+    }
+
+    override fun navigateTo(fragment: Fragment, addToBackStack: Boolean) {
+        val transaction = supportFragmentManager.beginTransaction()
+            .replace(R.id.container_main, fragment)
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/taskandroid/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.taskandroid
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
